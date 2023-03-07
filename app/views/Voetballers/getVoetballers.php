f46837b2ec1743535085a38b9c992619652c7a3c
KaiOsU8 <kokokovachev10@gmail.com>
2023-03-07 22:23:39 +0100

    commiitng again!

--- app/views/Voetballers/getVoetballers.php
+++ app/views/Voetballers/getVoetballers.php
@@ -4,18 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="http://mvc-oop-proeftoets.com/public/css/style.css">
     <title>Voetballers</title>
 </head>
 <body>
-    <h3><?= $data['title']; ?></h3>
+    <h3 class="title1"><?= $data['title']; ?></h3>
 
-    <table border='1'>
-        <thead>
-            <th>Naam</th>
-            <th>Club</th>
-            <th>Leeftijd</th>
-            <th>Nationaliteit</th>
-            <th>Salaris</th>
+    <table>
+        <thead class="around">
+            <th class="naam">Naam</th>
+            <th class="club">Club</th>
+            <th class="leeftijd">Leeftijd</th>
+            <th class="nationaliteit">Nationaliteit</th>
+            <th class="salaris">Salaris (Miljoen)</th>
         </thead>
         <tbody>
             <?= $data['tableRows']; ?>
